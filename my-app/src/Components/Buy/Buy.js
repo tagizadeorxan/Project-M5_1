094b987b1efc968591d88b9f57a3d1ba88ca9338
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-04-30 18:53:55 +0400

    ok

--- my-app/src/Components/Buy/Buy.js
+++ my-app/src/Components/Buy/Buy.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
+import './Buy';
 
 class Buy extends Component {
 
-state = {data:{symbol:'',profile:{price:'',beta:'',image:'',companyName:''}}};
+state = {data:{symbol:'',profile:{price:'',companyName:''}},value:1,price:0};
 
 constructor(props){
     super(props);
     this.getData();
+}
+
+handleMinus = () => {
+   let result = this.state.value>1? this.setState({value:this.state.value-1}): null;
+}
+
+handlePlus = () =>{
+    this.setState({value:this.state.value+1});
+}
+
+handleBuy = () => {
+
+    let {data} = this.state;
+
+    let price = this.state.value*data.profile.price;
+    this.setState({price});
+    let options = {
+        method:'POST',
+        
+
+
+        body: JSON.stringify({
+            code: data.symbol,
+            amount: data.profile.price,
+            purchasePrice: data.price
+        })
+       
+    }
+    console.log(options);
+
+    fetch("https://5e8da89e22d8cd0016a798db.mockapi.io/users/4/stocks",options);
 }
 
 
@@ -14,7 +46,12 @@ constructor(props){
 getData = () => {
     let id = this.props.match.params.id;
     console.log(this.props);
-    fetch(`https://financialmodelingprep.com/api/v3/company/profile/${id}`).then(data=>data.json()).then(data=>this.setState({data}));
+    fetch(`https://financialmodelingprep.com/api/v3/company/profile/${id}`).then(data=>data.json()).then(data=>{
+        
+    let price = data.profile.price;
+    
+    this.setState({data,price})
+    });
 }
 
     render() {
@@ -25,8 +62,12 @@ getData = () => {
                 <button onClick={()=>this.props.history.goBack()}>Back</button>
                <p>{data.profile.companyName}</p>
                <p>{data.profile.price}</p>
-               <img atl="id" src={data.profile.image}/>
-    
+                <div>
+                    <button onClick={this.handleMinus}>-</button>
+                    <span>{this.state.value}</span>
+                    <button onClick={this.handlePlus}>+</button>
+                </div>
+                <button onClick={this.handleBuy}>Buy</button>
             </div>
            
         )
