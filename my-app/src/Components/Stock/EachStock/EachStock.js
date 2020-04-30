5d3396df1535ef258727dba8832d2f7f07071683
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-04-30 14:15:21 +0400

    ok

--- my-app/src/Components/Stock/EachStock/EachStock.js
+++ my-app/src/Components/Stock/EachStock/EachStock.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import './eachstock.css';
 
-let data = {
-    symbol: "AVGO",
-    name: "Broadcom Limited",
-    price: 276.02,
-}
+
 
 
 class EachStock extends Component {
 
 
     render() {
-        // let {data} = this.props; 
+        let {data} = this.props; 
         let splitstring = String(data.price).split('.')
 
         return (
             <div className="eachStock-row">
                 <div className="eachStock-row-symbol">{data.symbol}</div>
-                <div className="eachStock-row-name">{data.name}</div>
+                <div className="eachStock-row-name"><div>{data.name}</div></div>
+                
                 <div className="eachStock-row-price">{splitstring[0]}.</div>
                 <div className="eachStock-row-price-decimal">{splitstring[1]} $</div>
             </div>
