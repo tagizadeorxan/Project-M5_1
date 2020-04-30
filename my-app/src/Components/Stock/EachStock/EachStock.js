0cb96cc1da822aa5de3f95ad42cb878d46e8dff7
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-04-30 15:32:12 +0400

    eachstock

--- my-app/src/Components/Stock/EachStock/EachStock.js
+++ my-app/src/Components/Stock/EachStock/EachStock.js
@@ -14,14 +14,18 @@ class EachStock extends Component {
         let splitstring = String(data.price).split('.')
 
         return (
-            <div className="eachStock-row">
+           
+            <Link className="link" to={`/stock/${data.symbol}`}>
+           
+           
+           <div className="eachStock-row">
             <div className="eachStock-row-symbol"> <div >{data.symbol}</div></div>
                 <div className="eachStock-row-name"><div>{data.name}</div></div>
                 <div className="eachStock-row-price">{splitstring[0]}.</div>
                 <div className="eachStock-row-price-decimal">{splitstring[1]} $</div>
 
-                <Link to={`/stock/${data.symbol}`}>buy</Link>
-            </div>
+                
+            </div></Link>
 
 
         )
