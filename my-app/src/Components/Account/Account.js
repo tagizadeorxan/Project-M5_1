107808d98dfa038d37b867b2af193b7a9adf166b
Elkhan <is.elxan@gmail.com>
2020-04-30 21:16:22 +0400

    Fix pagination

--- my-app/src/Components/Account/Account.js
+++ my-app/src/Components/Account/Account.js
@@ -22,22 +22,30 @@ class Account extends Component {
     }
 
     getAccountData = async() => {
-        const response = await fetch('');
+        const response = await fetch('https://5e8da89e22d8cd0016a798db.mockapi.io/users/4/stocks');
         const data = await response.json();
-        this.setState({data});
+        this.setState({data}, this.globalAccFind);
+    }
 
+    globalAccFind = () => {
+        console.log(this.state.data);
     }
 
     render() {
+        const {globalAccountSum, globalSumChange, globalPercentChange} = this.state;
         return (
             <div>
                 <div className="Account-header">
-                    <h1 className="Account-header-center"><span>{this.state.globalAccountSum}</span>$</h1>
-                    <h2 className="Account-header-center"><ShowDiffer data={{first: this.state.globalSumChange, second: this.state.globalPercentChange}} /></h2>
+                    <h1 className="Account-header-center"><span>{globalAccountSum}</span>$</h1>
+                    <h2 className="Account-header-center">
+                        <ShowDiffer data={{first: globalSumChange, second: globalPercentChange}} />
+                    </h2>
                 </div>   
 
                 <div className="Account-content">
-                      
+                      {
+                          
+                      }
                 </div>
             </div>
         )
