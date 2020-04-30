eb323b938f79196c684d09d04c2b0e3f9e51f390
Elkhan <is.elxan@gmail.com>
2020-04-30 17:12:44 +0400

    add footer

--- my-app/src/Components/Balance/Balance.js
+++ my-app/src/Components/Balance/Balance.js
@@ -1,11 +1,32 @@
 import React,{Component} from 'react';
+import './Balance.css';
 
 class Balance extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            balance: 0
+        }
+    }
+
+
+    componentDidMount() {
+        // this.getBalance();
+        setInterval(this.getBalance, 5000);
+    }
+
+    getBalance = async() => {
+        const response = await fetch('https://5e8da89e22d8cd0016a798db.mockapi.io/users/4');
+        const data = await response.json();
+        this.setState({balance: data.currentBalance});
+    }
 
     render() {
         return (
-            <footer>
-                
+            <footer className="Balance-footer">
+                <div>Balance:</div>
+                <div>{this.state.balance.toFixed(2)}$</div>
             </footer>
         )
     }
