ca1eb1b2a3a891b710287d3412f9e648509816ac
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-04-30 14:40:50 +0400

    eachstock

--- my-app/src/Components/Stock/EachStock/EachStock.js
+++ my-app/src/Components/Stock/EachStock/EachStock.js
@@ -13,9 +13,8 @@ class EachStock extends Component {
 
         return (
             <div className="eachStock-row">
-                <div className="eachStock-row-symbol">{data.symbol}</div>
+            <div className="eachStock-row-symbol"> <div >{data.symbol}</div></div>
                 <div className="eachStock-row-name"><div>{data.name}</div></div>
-                
                 <div className="eachStock-row-price">{splitstring[0]}.</div>
                 <div className="eachStock-row-price-decimal">{splitstring[1]} $</div>
             </div>
